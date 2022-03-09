1cf8566997dc3ef0a0ac092418b952b4df6b169c
Charli <huetercharlotte@gmail.com>
2022-03-09 14:59:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+<script>
+  
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -26,3 +28,5 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+</script>
